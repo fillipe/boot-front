13289a9971467716518d767f356520840680145a
Fillipe Aguiar Soares <fillipeaguiar.soares@gmail.com>
2018-07-04 20:42:08 -0300

    Primeiro commit

---
+++ src/main/java/br/com/fill/samples/bootfront/BootFrontApplication.java
@@ -0,0 +1,12 @@
+package br.com.fill.samples.bootfront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootFrontApplication.class, args);
+	}
+}
