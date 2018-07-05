63982aecd3262709fd5dabb5e453453b5f11b6d9
Vitor Oliveira <vtr@asap.inf.br>
2018-07-04 23:21:41 -0300

    Correção das merda da Phill e scripts ansible

--- src/main/java/br/com/fill/samples/bootfront/BootFrontApplication.java
+++ src/main/java/br/com/fill/samples/bootfront/BootFrontApplication.java
@@ -2,6 +2,9 @@ package br.com.fill.samples.bootfront;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import br.com.fill.samples.bootfront.controller.HomeController;
 
 @SpringBootApplication
 public class BootFrontApplication {
--- src/main/java/br/com/fill/samples/bootfront/controller/HomeController.java
+++ src/main/java/br/com/fill/samples/bootfront/controller/HomeController.java
@@ -1,5 +1,13 @@
 package br.com.fill.samples.bootfront.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HomeController {
 
+    @GetMapping(value = "/arara")
+    public String arara() {
+        return "aaaaaaaaaaaaa";
+    }
 }
